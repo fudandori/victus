1cf26713f8e216cb6b62c8b6adda8caaf829eb23
fudandori <fudandori@gmail.com>
2024-10-20 15:34:46 +0200

    initial values

---
+++ js/values.js
@@ -0,0 +1,42 @@
+const food = {
+    tomate_tamizado: {
+        calories: 35,
+        fat: 0.2,
+        fat_sat: 0,
+        carbs: 6.6,
+        sugar: 3.8,
+        prot: 1.4,
+        fiber: 0,
+        salt: 0.1
+    },
+    pechuga_pavo_92percent: {
+        calories: 89,
+        fat: 1.3,
+        fat_sat: 0.5,
+        carbs: 0,
+        sugar: 0,
+        prot: 19,
+        fiber: 0,
+        salt: 1.9
+    },
+    filetes_pavo: {
+        calories: 103,
+        fat: 0.9,
+        fat_sat: 0.3,
+        carbs: 0,
+        sugar: 0,
+        prot: 24,
+        fiber: 0,
+        salt: 0.26
+    },
+    yogur: {
+        calories: 35,
+        fat: 0.1,
+        fat_sat: 0,
+        carbs: 4.4,
+        sugar: 4.4,
+        prot: 4.3,
+        fiber: 0,
+        salt: 0.13
+    }
+}
